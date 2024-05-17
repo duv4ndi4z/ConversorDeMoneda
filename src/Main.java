14fd06374d187aa1a79ae265be957ef12bf01fb0
Duvan Diaz <159075852+duv4ndi4z@users.noreply.github.com>
2024-05-16 21:39:10 -0500

    First Version

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Double numberToConvert;
+        double numberToConvert;
         String currA="";
         String currB="";
 
